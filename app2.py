62c3d4c5f2373a44048a86633e22b247b04b57b6
kumarkse <abhishektiwari2100@gmail.com>
2024-05-07 02:49:17 +0530

    no change

--- app2.py
+++ app2.py
@@ -37,22 +37,23 @@ selected_movie = st.selectbox(
 
 if st.button('Show Recommendation'):
     recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.columns(5,gap="large")
+    col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0],width=100)
+        st.image(recommended_movie_posters[0])
     with col2:
         st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1],width=100)
+        st.image(recommended_movie_posters[1])
+
     with col3:
         st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2],width=100)
+        st.image(recommended_movie_posters[2])
     with col4:
         st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3],width=100)
+        st.image(recommended_movie_posters[3])
     with col5:
         st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4],width=100)
+        st.image(recommended_movie_posters[4])
 
 
 
